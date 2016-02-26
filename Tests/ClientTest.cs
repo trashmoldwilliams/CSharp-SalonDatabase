8d487ae7e723cc127d45119e192c7524e2821923
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-26 08:29:32 -0800

    Pass general testing for Stylist class.

--- Tests/ClientTest.cs
+++ Tests/ClientTest.cs
@@ -1,17 +1,17 @@
-using Xunit;
-using System.Collections.Generic;
-using System;
-using System.Data;
-using System.Data.SqlClient;
-
-namespace HairSalonNS
-{
-  public class ClientTest : IDisposable
-  {
-    public ClientTest()
-    {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
-
-    }
-  }
-}
+// using Xunit;
+// using System.Collections.Generic;
+// using System;
+// using System.Data;
+// using System.Data.SqlClient;
+//
+// namespace HairSalonNS
+// {
+//   public class ClientTest : IDisposable
+//   {
+//     public ClientTest()
+//     {
+//       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+//
+//     }
+//   }
+// }
