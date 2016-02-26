6980072363b4edb0b757b05234aa9e051e3c4fd6
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-26 10:12:46 -0800

    Add views for clients pertaining to stylists, and forms for adding new clients.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -26,10 +26,35 @@ namespace HairSalonNS
       };
 
       Get["/stylists/delete_all"] = _ => {
-          Stylist.DeleteAll();
-          List<Stylist> AllStylists = Stylist.GetAll();
-          return View["stylists.cshtml", AllStylists];
-        };
+        Stylist.DeleteAll();
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
+
+      Get["/stylists/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var SelectedStylist = Stylist.Find(parameters.id);
+        var StylistClient = SelectedStylist.GetClients();
+        List<Stylist> AllStylists = Stylist.GetAll();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClient);
+        model.Add("stylists", AllStylists);
+        return View["clients.cshtml", model];
+      };
+
+      Post["/stylists/{id}/new"] = parameters => {
+        Client newClient = new Client(Request.Form["name"], Request.Form["stylist-id"]);
+        newClient.Save();
+
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var SelectedStylist = Stylist.Find(parameters.id);
+        var StylistClient = SelectedStylist.GetClients();
+        List<Stylist> AllStylists = Stylist.GetAll();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClient);
+        model.Add("stylists", AllStylists);
+        return View["clients.cshtml", model];
+      };
     }
   }
 }
