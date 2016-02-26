87570b5b172fc5b8962cbe1b226667551b17c3ed
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-26 08:55:39 -0800

    Add view for stylists, and paths for addition and deletion.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,7 +12,24 @@ namespace HairSalonNS
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
-    }
 
+      Get["/stylists"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
+
+      Post["/stylists/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
+
+      Get["/stylists/delete_all"] = _ => {
+          Stylist.DeleteAll();
+          List<Stylist> AllStylists = Stylist.GetAll();
+          return View["stylists.cshtml", AllStylists];
+        };
+    }
   }
 }
